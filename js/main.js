fb731a57afeb5cae94c0e05b16cd4b8da38b223b
MrSpudi <66726773+MrSpudi@users.noreply.github.com>
2020-07-02 14:44:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿$(function(){
+
+  $('.header-slider').slick({
+    arrows: false,
+    vertical: true,
+    dots: true,
+    dotsClass: 'header-dots',
+    autoplay: 2000,
+  });
+
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+a
+});
